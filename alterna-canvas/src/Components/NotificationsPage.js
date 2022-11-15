3cfa17e296291499990ac03c50cfb2c105714033
Myers747 <myerstb@mail.uc.edu>
2022-11-14 21:26:21 -0500

    Update NotificationsPage.js

--- alterna-canvas/src/Components/NotificationsPage.js
+++ alterna-canvas/src/Components/NotificationsPage.js
@@ -1,10 +1,63 @@
-import React from "react";
 
-function NotificationsPage() {
-  return (
-    <div className="main-content">
-      <h1>Notifications Placeholder</h1>
-    </div>
-  );
+import React from 'react'
+import 'react-notifications/lib/notifications.css';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { Helmet } from "react-helmet";
+import 'bootstrap/dist/css/bootstrap.css';
+import Course from "./Course";
+import TodoList from "./TodoList";
+import Info from "./Info";
+import {ListItem, List, ListItemText, Typography, Divider} from '@mui/material';
+
+
+
+class Example extends React.Component {
+  createNotification = (type) => {
+    return () => {
+      switch (type) {
+        case 'info':
+          NotificationManager.info('Info message');
+          break;
+        case 'success':
+          NotificationManager.success('Graded: Getting to know you', '');
+          break;
+        case 'warning':
+          NotificationManager.warning('Warning not message', 'Close after 3000ms', 3000);
+          break;
+        case 'error':
+          NotificationManager.error('Error message', 'Click me!', 5000, () => {
+            alert('callback');
+          });
+          break;
+      }
+    };
+  };
+
+
+
+  render() {
+    return (
+      <div class="home-content">
+        <button className='btn btn-info'
+          onClick={this.createNotification('info')}>Info
+        </button>
+        <hr/>
+        <button className='btn btn-success'
+          onClick={this.createNotification('success')}>Success
+        </button>
+        <hr/>
+        <button className='btn btn-warning'
+          onClick={this.createNotification('warning')}>Warning
+        </button>
+        <hr/>
+        <button className='btn btn-danger'
+          onClick={this.createNotification('error')}>Error
+        </button>
+
+        <NotificationContainer/>
+      </div>
+    );
+  }
 }
-export default NotificationsPage;
+
+export default Example;
